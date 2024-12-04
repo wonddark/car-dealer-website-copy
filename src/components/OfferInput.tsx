66a395d2b3ecad60a474cff94e555275ff4de43a
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-04 15:11:32 +0400

    Allow user to submit its offer

--- src/components/OfferInput.tsx
+++ src/components/OfferInput.tsx
@@ -12,30 +12,47 @@ export default function OfferInput({
     vehicle.currentOffer + OFFER_STEP,
   );
   return (
-    <div className="hstack">
-      <button
-        className="btn btn-outline-primary"
-        type="button"
-        onClick={() => setMyOffer((prevState) => prevState - OFFER_STEP)}
-        disabled={myOffer === vehicle.currentOffer + OFFER_STEP}
-      >
-        <i className="ti ti-minus"></i>
-      </button>
-      <input
-        type="number"
-        min={vehicle.currentOffer + OFFER_STEP}
-        step={OFFER_STEP}
-        value={myOffer}
-        onChange={({ currentTarget: { value } }) => setMyOffer(Number(value))}
-        className="form-control"
-      />
-      <button
-        className="btn btn-outline-primary"
-        type="button"
-        onClick={() => setMyOffer((prevState) => prevState + OFFER_STEP)}
-      >
-        <i className="ti ti-plus"></i>
-      </button>
+    <div>
+      <div className="hstack">
+        <button
+          className="btn btn-outline-primary"
+          type="button"
+          onClick={() => setMyOffer((prevState) => prevState - OFFER_STEP)}
+          disabled={myOffer === vehicle.currentOffer + OFFER_STEP}
+        >
+          <i className="ti ti-minus"></i>
+        </button>
+        <input
+          type="number"
+          min={vehicle.currentOffer + OFFER_STEP}
+          step={OFFER_STEP}
+          value={myOffer}
+          onChange={({ currentTarget: { value } }) => setMyOffer(Number(value))}
+          className="form-control text-center"
+        />
+        <button
+          className="btn btn-outline-primary"
+          type="button"
+          onClick={() => setMyOffer((prevState) => prevState + OFFER_STEP)}
+        >
+          <i className="ti ti-plus"></i>
+        </button>
+      </div>
+      <div className="d-grid gap-1 mt-2">
+        <button type="button" className="btn btn-primary">
+          Publicar mi oferta
+        </button>
+        <p className="text-center small text-muted">
+          Todas las ofertas son legalmente vinculantes y se venden &#34;donde
+          esta y como esta&#34;.
+          <button
+            className="btn btn-link d-inline btn-sm"
+            onClick={() => console.log(myOffer)}
+          >
+            Obtener más información
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
