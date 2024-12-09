afabb4e014d73a2faa3b9bb4c205e212a419cd50
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-09 22:30:46 +0400

    Minor adjustments in label texts

--- src/components/ForgetPassword.tsx
+++ src/components/ForgetPassword.tsx
@@ -1,45 +1,43 @@
 import React from "react";
+import Link from "next/link";
 
 const ForgetPassword = () => {
   return (
-    <>
-      <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-10 col-lg-8">
+    <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
+      <div className="container mb-5">
+        <div className="row justify-content-center">
+          <div className="col-10 col-lg-8">
+            <Link href="/">
               <img
-                className="big-logo"
-                src="img/core-img/logo-white.png"
-                alt=""
+                src="/assets/img/core-img/logo-round.png"
+                alt="la subasta cubana"
+                className="mb-5"
               />
+            </Link>
 
-              <div className="register-form mt-5">
-                <form action="/forget-password-success" method="">
-                  <div className="form-group text-start mb-4">
-                    <span>Email or Username</span>
-                    <label htmlFor="email">
-                      <i className="ti ti-user"></i>
-                    </label>
-                    <input
-                      className="form-control"
-                      id="email"
-                      type="text"
-                      placeholder="Designing World"
-                    />
-                  </div>
-                  <button
-                    className="btn btn-warning btn-lg w-100"
-                    type="submit"
-                  >
-                    Reset Password
-                  </button>
-                </form>
-              </div>
+            <div className="register-form mt-5">
+              <form action="/forget-password-success" method="">
+                <div className="form-group text-start mb-4">
+                  <span>Correo electrónico</span>
+                  <label htmlFor="email">
+                    <i className="ti ti-user"></i>
+                  </label>
+                  <input
+                    className="form-control"
+                    id="email"
+                    type="text"
+                    placeholder="Designing World"
+                  />
+                </div>
+                <button className="btn btn-primary btn-lg w-100" type="submit">
+                  Restablecer contraseña
+                </button>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/ForgetPasswordSuccess.tsx
+++ src/components/ForgetPasswordSuccess.tsx
@@ -2,27 +2,25 @@ import Link from "next/link";
 
 const ForgetPasswordSuccess = () => {
   return (
-    <>
-      <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-10 col-lg-8">
-              <div className="success-check">
-                <i className="ti ti-mail-check"></i>
-              </div>
-
-              <p className="text-white mt-3 mb-4">
-                Password recovery email is sent successfully. Please check your
-                inbox!
-              </p>
-              <Link className="btn btn-warning btn-lg" href="/login">
-                Go Home
-              </Link>
+    <div className="login-wrapper d-flex align-items-center justify-content-center text-center">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-10 col-lg-8">
+            <div className="success-check">
+              <i className="ti ti-mail-check"></i>
             </div>
+
+            <p className="text-white mt-3 mb-4">
+              Se ha enviado un mensaje a tu bandeja de correo para restablecer
+              tu contraseña.
+            </p>
+            <Link className="btn btn-warning btn-lg" href="/">
+              Ir al inicio
+            </Link>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
