5a1054eb9f19f8738a514847c9015f8f93a13851
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-15 23:49:16 +0400

    Renamed the route param to identify a single vehicle
    -- From 'id' to 'vin'

--- src/app/(app-pages)/vehicles/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import VehicleDetails from "@/components/VehicleDetails";
-import top_product from "@/data/top_product";
-
-export default function VehicleDetailsPage() {
-  return <VehicleDetails vehicle={top_product[0]} />;
-}
