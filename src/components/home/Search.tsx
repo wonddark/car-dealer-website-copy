85b949510e89320fc92829d0a5409d868281ad27
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-15 23:54:06 +0400

    Removed the unused search input
    -- Plans exits for using this space for quick access to in use filters
    for clean up actions

--- src/components/home/Search.tsx
+++ src/components/home/Search.tsx
@@ -1,30 +1,15 @@
 "use client";
+
 import React, { useState } from "react";
 import OffCanvasTwo from "@/components/common/OffCanvasTwo";
-import { useSearchParams } from "next/navigation";
 
 const Search = () => {
-  const p = useSearchParams();
-  const query = p.get("makes") ?? "";
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(!show);
   return (
     <>
-      <div className="container">
+      <div className="container-xl">
         <div className="search-form pt-3 rtl-flex-d-row-r">
-          <form action="/vehicles/search">
-            <input
-              className="form-control"
-              type="search"
-              name="makes"
-              placeholder="Buscar vehículos"
-              defaultValue={query}
-            />
-            <button type="submit">
-              <i className="ti ti-search"></i>
-            </button>
-          </form>
-
           <div className="alternative-search-options">
             <div
               className="filter-option ms-2"
