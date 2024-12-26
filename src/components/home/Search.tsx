3ad3a00b6a108360d99dd076aa80bd04b14f2c22
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-27 03:04:30 +0400

    Implemented orderBy and sort

--- src/components/home/Search.tsx
+++ src/components/home/Search.tsx
@@ -4,11 +4,14 @@ import React, { useState } from "react";
 import OffCanvasTwo from "@/components/common/OffCanvasTwo";
 import useTopProducts from "@/components/home/use-top-products";
 import FiltersBanner from "@/components/FiltersBanner";
+import { useFilters } from "@/components/common/Filters";
 
 const Search = () => {
   const { response, loading } = useTopProducts();
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(!show);
+  const { handleFilterYearChange, orderBySelected, orderActive, changeSort } =
+    useFilters();
   return (
     <>
       <div className="container-xl py-3">
@@ -36,12 +39,79 @@ const Search = () => {
                 <FiltersBanner />
               </div>
               {!loading && response.totalCount > 0 && (
-                <p className="m-0">
-                  <strong className="text-primary-emphasis">
-                    {response.totalCount}
-                  </strong>{" "}
-                  vehículos encontrados
-                </p>
+                <div className="hstack justify-content-between">
+                  <p className="m-0">
+                    <strong className="text-primary-emphasis">
+                      {response.totalCount}
+                    </strong>{" "}
+                    vehículos encontrados
+                  </p>
+                  <div className="hstack gap-4">
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                      name="SortBy"
+                      onChange={handleFilterYearChange}
+                    >
+                      <option selected={orderBySelected("")} value="">
+                        Ordenar por
+                      </option>
+                      <option
+                        selected={orderBySelected("BuyNowPrice")}
+                        value="BuyNowPrice"
+                      >
+                        Precio de venta
+                      </option>
+                      <option
+                        selected={orderBySelected("Mileage")}
+                        value="Mileage"
+                      >
+                        Cantidad de millas
+                      </option>
+                      <option
+                        selected={orderBySelected("SaleDate")}
+                        value="SaleDate"
+                      >
+                        Fecha de subasta
+                      </option>
+                      <option
+                        selected={orderBySelected("HighBid")}
+                        value="HighBid"
+                      >
+                        Oferta actual
+                      </option>
+                      <option selected={orderBySelected("Make")} value="Make">
+                        Marca
+                      </option>
+                      <option selected={orderBySelected("Model")} value="Model">
+                        Modelo
+                      </option>
+                      <option selected={orderBySelected("Year")} value="Year">
+                        Año
+                      </option>
+                    </select>
+                    <div className="hstack">
+                      <button
+                        type="button"
+                        className={`btn btn-light${orderActive("false") ? " active" : ""}`}
+                        data-bs-toggle="button"
+                        aria-pressed={orderActive("false")}
+                        onClick={() => changeSort("false")}
+                      >
+                        <i className="ti ti-sort-ascending"></i>
+                      </button>
+                      <button
+                        type="button"
+                        className={`btn btn-light${orderActive("true") ? " active" : ""}`}
+                        data-bs-toggle="button"
+                        aria-pressed={orderActive("true")}
+                        onClick={() => changeSort("true")}
+                      >
+                        <i className="ti ti-sort-descending"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
               )}
             </div>
           </div>
