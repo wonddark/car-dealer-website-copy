16b797ce63937b3aac86327e32a82166ad46777f
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-28 20:19:03 +0400

    Reduced the number of items to be shown in sections BuyNow and MostWanted

--- src/hooks/useMostWanted.ts
+++ src/hooks/useMostWanted.ts
@@ -1,49 +1,36 @@
 import { useEffect, useState } from "react";
 import { Vehicle } from "@/types/vehicle";
 
-export const useBuyNow = () => {
+export const useMostWanted = () => {
   const [response, setResponse] = useState<{
     data: Vehicle[];
     loading: boolean;
     error: boolean;
-  }>({
-    data: [],
-    loading: true,
-    error: false,
-  });
-
-  const getNextPage = () => {
+  }>({ data: [], loading: true, error: false });
+  const getData = () => {
     const controller = new AbortController();
     fetch(
       process.env.NEXT_PUBLIC_DOMAIN +
-        "/api/inventory/search?HasBuyNowPrice=true&PageNumber=1&PageSize=4",
+        "/api/inventory/search?PageNumber=1&PageSize=4&IsBestOffer=true",
       { signal: controller.signal },
     )
       .then((res) => res.json())
-      .then((res) =>
-        setResponse({
-          data: res.data,
-          loading: false,
-          error: false,
-        }),
-      )
+      .then((res) => {
+        setResponse({ data: res.data, loading: false, error: false });
+      })
       .catch((reason) => {
         if (reason instanceof DOMException && reason.name === "AbortError") {
           return null;
         } else {
-          setResponse({
-            data: [],
-            error: true,
-            loading: false,
-          });
+          setResponse({ data: [], error: true, loading: false });
         }
       });
+
     return () => {
       controller.abort();
     };
   };
 
-  useEffect(getNextPage, []);
-
+  useEffect(getData, []);
   return { response };
 };
