9123c0d7466fd0f0112143404356e65a741ae1c0
Osmanys Fuentes L <ossmanys@gmail.com>
2024-12-28 20:20:42 +0400

    Created/updated endpoints for filters lookups

--- src/app/api/filters/vehicle-type/route.ts
+++ src/app/api/filters/vehicle-type/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
   try {
-    const res = await fetch(`${process.env.API_ENDPOINT}/lookups/vehicleTypes`);
+    const res = await fetch(`${process.env.API_ENDPOINT}/lookups/VehicleTypes`);
     const data = await res.json();
     return Response.json(data);
   } catch (e) {
