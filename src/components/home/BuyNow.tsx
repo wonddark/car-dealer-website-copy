d4f1bc0c96833c16942bcd80de0854df04230a1d
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-01 19:50:33 +0400

    Avoided a compile time error due to wrong response from API

--- src/components/home/BuyNow.tsx
+++ src/components/home/BuyNow.tsx
@@ -9,36 +9,32 @@ export default async function BuyNow() {
       process.env.API_ENDPOINT +
         "/auction-inventories/search?HasBuyNowPrice=true&PageNumber=1&PageSize=4",
     );
-    const data = (await res.json()) as VehicleResponse;
-    return (
-      <section className="pt-2">
-        <h5 className="ps-1">Comprar ahora</h5>
-        <div className="row g-2">
-          {data.data.map((item) => (
-            <div key={item.vin} className="col-12 col-md-6 col-xl-3">
-              <VehicleCard vehicle={item} />
-            </div>
-          ))}
-        </div>
-        <div className="hstack justify-content-end mt-1">
-          <Link
-            href="/vehicles?HasBuyNowPrice=true&IsBestOffer=false"
-            className="btn btn-light"
-          >
-            <span>Ver listado completo </span>
-            <i className="ti ti-chevron-right"></i>
-          </Link>
-        </div>
-      </section>
-    );
+    if (res.status === 200) {
+      const data = (await res.json()) as VehicleResponse;
+      return (
+        <section className="pt-2">
+          <h5 className="ps-1">Comprar ahora</h5>
+          <div className="row g-2">
+            {data.data.map((item) => (
+              <div key={item.vin} className="col-12 col-md-6 col-xl-3">
+                <VehicleCard vehicle={item} />
+              </div>
+            ))}
+          </div>
+          <div className="hstack justify-content-end mt-1">
+            <Link
+              href="/vehicles?HasBuyNowPrice=true&IsBestOffer=false"
+              className="btn btn-light"
+            >
+              <span>Ver listado completo </span>
+              <i className="ti ti-chevron-right"></i>
+            </Link>
+          </div>
+        </section>
+      );
+    }
+    return <p>Ocurrió un error, por favor vuelve a intentarlo.</p>;
   } catch (e) {
-    console.error(
-      "THIS IS THE ERROR",
-      e,
-      "URL",
-      process.env.DOMAIN +
-        "/api/inventory/search?HasBuyNowPrice=true&PageNumber=1&PageSize=4",
-    );
-    return <>Error</>;
+    return <p>Ocurrió un error, por favor vuelve a intentarlo.</p>;
   }
 }
