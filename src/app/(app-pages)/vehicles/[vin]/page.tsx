6469fff420249174874f3de8e7ec6e80eb98fd3d
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-07 00:17:21 +0400

    Fixed the handling of errors during the generation of static data

--- src/app/(app-pages)/vehicles/[vin]/page.tsx
+++ src/app/(app-pages)/vehicles/[vin]/page.tsx
@@ -1,3 +1,4 @@
+import NotFound from "@/app/not-found";
 import VehicleDetails from "@/components/VehicleDetail/VehicleDetails";
 import { Vehicle } from "@/types/vehicle";
 
@@ -6,14 +7,22 @@ type Props = {
 };
 
 export default async function VehicleDetailsPage(props: Readonly<Props>) {
-  const {
-    params: { vin },
-  } = props;
-  const data = await fetch(
-    process.env.API_ENDPOINT + `/auction-inventories?vin=${vin}`,
-  ).then((res) => res.json());
+  try {
+    const {
+      params: { vin },
+    } = props;
+    const data = await fetch(
+      process.env.API_ENDPOINT + `/auction-inventories?vin=${vin}`,
+    ).then((res) => res.json());
+    const odometerBrands = await fetch(
+      process.env.API_ENDPOINT + "/lookups/OdometerBrands",
+    ).then((res) => res.json());
 
-  return <VehicleDetails vehicle={data} />;
+    return <VehicleDetails vehicle={data} odometerBrands={odometerBrands} />;
+  } catch (e) {
+    console.error(e);
+    return <NotFound />;
+  }
 }
 
 export async function generateStaticParams(): Promise<{ vin: string }[]> {
@@ -29,8 +38,8 @@ export async function generateStaticParams(): Promise<{ vin: string }[]> {
 
     return Array.of(
       ...new Set([
-        ...buyNow.data.map((item: Vehicle) => item.vin),
-        ...mostWanted.data.map((item: Vehicle) => item.vin),
+        ...(buyNow.data?.map((item: Vehicle) => item.vin) ?? []),
+        ...(mostWanted.data?.map((item: Vehicle) => item.vin) ?? []),
       ]),
     ).map((item) => ({ vin: item }));
   } catch (e) {
@@ -40,15 +49,20 @@ export async function generateStaticParams(): Promise<{ vin: string }[]> {
 }
 
 export async function generateMetadata(props: Readonly<Props>) {
-  const {
-    params: { vin },
-  } = props;
-  const vehicle = await fetch(
-    process.env.API_ENDPOINT + `/auction-inventories?vin=${vin}`,
-  ).then((res) => res.json());
+  try {
+    const {
+      params: { vin },
+    } = props;
+    const vehicle = await fetch(
+      process.env.API_ENDPOINT + `/auction-inventories?vin=${vin}`,
+    ).then((res) => res.json());
 
-  return {
-    title: `${vehicle.year} ${vehicle.make} ${vehicle.model}`,
-    description: `${vehicle.year} ${vehicle.make} ${vehicle.model} subasta carros SUVs camionetas compra inmediata florida`,
-  };
+    return {
+      title: `${vehicle.year} ${vehicle.make} ${vehicle.model}`,
+      description: `${vehicle.year} ${vehicle.make} ${vehicle.model} subasta carros SUVs camionetas compra inmediata florida`,
+    };
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
 }
