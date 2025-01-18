80b5b195d4200a7b9026e0ceac0fa5bda5440fa3
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-18 16:29:13 +0400

    Changed this api to get damage translations instead of damage list

--- src/app/api/filters/damage-translations/route.ts
+++ src/app/api/filters/damage-translations/route.ts
@@ -1,15 +1,11 @@
 export async function GET() {
   try {
     const res = await fetch(
-      `${process.env.API_ENDPOINT}/lookups/PrimaryDamages`,
+      `${process.env.API_ENDPOINT}/lookups/Damages_Spanish`,
     );
     if (res.status === 200) {
       const data = await res.json();
-      return Response.json(
-        data.filter(
-          (item: string) => !item.includes("/") && !item.includes(" - "),
-        ),
-      );
+      return Response.json(data);
     }
     return Response.json([]);
   } catch (e) {
