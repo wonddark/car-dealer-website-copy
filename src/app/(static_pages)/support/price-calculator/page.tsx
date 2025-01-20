3aa5f7ec0fc54689e8815750fd39d5ec43fd5229
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-21 00:37:41 +0400

    Now in the footer we have two new links:
    -- Calculadora: it points to a page where we will implement the price calculator
    -- FAQ: it points to the frequently asked questions page (to be implemented)

---
+++ src/app/(static_pages)/support/price-calculator/page.tsx
@@ -0,0 +1,3 @@
+export default function PriceCalculator() {
+  return <div>TODO</div>;
+}
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
       <div className="suha-footer-nav">
         <ul className="h-100 d-flex align-items-center justify-content-between ps-0 d-flex rtl-flex-d-row-r">
           <li>
-            <Link href="/" style={{ pointerEvents: "none" }}>
-              <i className="ti ti-basket"></i>Cart
+            <Link href="/support/price-calculator">
+              <i className="ti ti-calculator"></i>Calculadora
             </Link>
           </li>
           <li>
@@ -27,8 +27,8 @@ const Footer = () => {
             </Link>
           </li>
           <li>
-            <Link href="/settings">
-              <i className="ti ti-help"></i>Ayuda
+            <Link href="/support/faq">
+              <i className="ti ti-help"></i>FAQ
             </Link>
           </li>
         </ul>
