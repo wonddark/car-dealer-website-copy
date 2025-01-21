2098206543af877d56e4c1ab87565a64b8a62553
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-22 01:03:30 +0400

    Added the FAQ page

--- src/app/(static_pages)/support/faq/page.tsx
+++ src/app/(static_pages)/support/faq/page.tsx
@@ -1,3 +1,28 @@
+import { FAQs } from "@/data/faqs";
+import * as Accordion from "@radix-ui/react-accordion";
+import { Card, CardBody, CardHeader } from "react-bootstrap";
+
 export default function FAQ() {
-  return <div>TODO</div>;
+  return (
+    <section>
+      <Accordion.Root collapsible type="single">
+        <div className="vstack gap-2">
+          {FAQs.map((item) => (
+            <Accordion.Item key={item.name} value={item.name}>
+              <Card>
+                <CardHeader className="p-0">
+                  <Accordion.Trigger className="btn btn-ligth text-start w-100">
+                    {item.name}
+                  </Accordion.Trigger>
+                </CardHeader>
+                <Accordion.Content>
+                  <CardBody>{item.acceptedAnswer.text}</CardBody>
+                </Accordion.Content>
+              </Card>
+            </Accordion.Item>
+          ))}
+        </div>
+      </Accordion.Root>
+    </section>
+  );
 }
