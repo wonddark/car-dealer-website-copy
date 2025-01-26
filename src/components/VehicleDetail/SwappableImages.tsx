0c8a6eb2d02abfa8356a52fcbdb1ce30924cd6a9
Osmanys Fuentes L <ossmanys@gmail.com>
2025-01-26 16:00:34 +0400

    Removed row/col containers and also extra paddings

--- src/components/VehicleDetail/SwappableImages.tsx
+++ src/components/VehicleDetail/SwappableImages.tsx
@@ -13,65 +13,61 @@ export default function SwappableImages({
   const hasImages = (vehicle.imageUrls ?? []).length > 0;
 
   return (
-    <div className="card-body">
-      <div className="row">
-        {hasImages ? (
-          <Swiper
-            modules={[Pagination, Controller]}
-            className="swiper col-12"
-            slidesPerView="auto"
-            onSwiper={setControl1}
-            controller={{ control: control2 }}
-            zoom={true}
-          >
-            {vehicle.imageUrls?.map((item) => (
-              <SwiperSlide key={item}>
-                <img
-                  src={item}
-                  className="object-fit-cover"
-                  alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        ) : (
-          <img
-            src={vehicle.imageUrl}
-            className="object-fit-cover"
-            alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
-          />
-        )}
-        {hasImages ? (
-          <div className="col-12 py-1">
-            <Swiper
-              modules={[FreeMode, Pagination, Controller]}
-              freeMode={{
-                enabled: true,
-                momentum: true,
-                momentumRatio: 0.9,
-                momentumVelocityRatio: 0.9,
-              }}
-              className="swiper"
-              slidesPerView="auto"
-              onSwiper={setControl2}
-              controller={{ control: control1 }}
-            >
-              {vehicle.imageUrls?.map((item) => (
-                <SwiperSlide key={item}>
-                  <img
-                    src={item}
-                    style={{
-                      height: 64,
-                      objectFit: "cover",
-                    }}
-                    alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-        ) : null}
-      </div>
-    </div>
+    <>
+      {hasImages ? (
+        <Swiper
+          modules={[Pagination, Controller]}
+          className="swiper"
+          slidesPerView="auto"
+          onSwiper={setControl1}
+          controller={{ control: control2 }}
+          zoom={true}
+        >
+          {vehicle.imageUrls?.map((item) => (
+            <SwiperSlide key={item}>
+              <img
+                src={item}
+                className="object-fit-cover"
+                alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <img
+          src={vehicle.imageUrl}
+          className="object-fit-cover"
+          alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
+        />
+      )}
+      {hasImages ? (
+        <Swiper
+          modules={[FreeMode, Pagination, Controller]}
+          freeMode={{
+            enabled: true,
+            momentum: true,
+            momentumRatio: 0.9,
+            momentumVelocityRatio: 0.9,
+          }}
+          className="swiper pt-1"
+          slidesPerView="auto"
+          onSwiper={setControl2}
+          controller={{ control: control1 }}
+        >
+          {vehicle.imageUrls?.map((item) => (
+            <SwiperSlide key={item}>
+              <img
+                src={item}
+                style={{
+                  height: 64,
+                  objectFit: "cover",
+                }}
+                alt={`${vehicle.year}${vehicle.make}${vehicle.model} subasta cubana comprar carros baratos`}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : null}
+    </>
   );
 }
