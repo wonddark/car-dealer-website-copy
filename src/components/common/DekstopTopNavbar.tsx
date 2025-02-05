9568ffd992e8c71ec4f56526340dc3bc758838e5
Osmanys Fuentes L <ossmanys@gmail.com>
2025-02-05 21:53:26 +0400

    Redesigned the navbar menu

--- src/components/common/DekstopTopNavbar.tsx
+++ src/components/common/DekstopTopNavbar.tsx
@@ -2,7 +2,7 @@
 
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
-import { Button, Card, CardBody, Col, Row } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import React from "react";
 import { usePathname } from "next/navigation";
 import Populars from "@/components/common/Populars";
@@ -25,49 +25,41 @@ export default function DesktopTopNavbar() {
         <NavigationMenu.Item>
           <NavigationMenu.Trigger asChild>
             <NavigationMenu.Link asChild>
-              <Button
-                variant={pathname.includes("/vehicles") ? "primary" : "link"}
-                className={`d-inline-flex align-items-center gap-1 text-decoration-none${pathname.includes("/vehicles") ? " rounded-bottom-0" : ""}`}
+              <Link
+                href="/vehicles?IsBestOffer=true"
+                className={`btn shit-class d-inline-flex align-items-center gap-1 text-decoration-none ${pathname.includes("/vehicles") ? "btn-primary rounded-bottom-0" : "btn-link"}`}
               >
                 <i className="ti ti-car-garage"></i>
                 <span>Vehículos</span>
-              </Button>
+              </Link>
             </NavigationMenu.Link>
           </NavigationMenu.Trigger>
           <NavigationMenu.Content className="position-absolute">
-            <Card
-              style={{
-                maxWidth: "75vw",
-                left: 0,
-              }}
-            >
-              <CardBody>
-                <NavigationMenu.Link asChild>
-                  <Row>
-                    <Col xs={12}>
-                      <Link
-                        href="/vehicles?IsBestOffer=true"
-                        className="btn btn-link text-decoration-none px-0"
-                      >
-                        Inventario
-                      </Link>
-                    </Col>
-                    <Col xs={12}>
-                      <span>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Ab accusamus accusantium aliquid aperiam aut
-                        deserunt dolor dolorem, ipsa nisi nobis pariatur, porro
-                        quas qui recusandae reprehenderit repudiandae sit soluta
-                        voluptate?
-                      </span>
-                    </Col>
-                  </Row>
-                </NavigationMenu.Link>
-                <hr className="my-3" />
-                <Populars />
-              </CardBody>
+            <Card className="shadow rounded-0">
+              <NavigationMenu.Sub>
+                <NavigationMenu.List className="m-0 p-0 d-flex flex-column align-items-stretch">
+                  <NavigationMenu.Item asChild>
+                    <Button variant="link" className="text-decoration-none">
+                      <NavigationMenu.Link asChild>
+                        <Link
+                          href="/vehicles?IsBestOffer=true"
+                          className="d-flex gap-3 align-items-center fw-normal"
+                        >
+                          Todos los vehículos
+                        </Link>
+                      </NavigationMenu.Link>
+                    </Button>
+                  </NavigationMenu.Item>
+                  <Populars />
+                </NavigationMenu.List>
+              </NavigationMenu.Sub>
             </Card>
           </NavigationMenu.Content>
+
+          <NavigationMenu.Viewport
+            className="position-absolute"
+            style={{ top: "40wpx", left: 0 }}
+          />
         </NavigationMenu.Item>
         <NavigationMenu.Item>
           <NavigationMenu.Link asChild>
@@ -101,7 +93,6 @@ export default function DesktopTopNavbar() {
           </NavigationMenu.Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
-      <NavigationMenu.Viewport />
     </NavigationMenu.Root>
   );
 }
